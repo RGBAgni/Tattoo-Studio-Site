ced813d1739322bcefb78579474576c4f0c23d2a
RGBAgni <aarchipovaite@gmail.com>
2025-04-18 17:13:45 +0200

    added comments
    added comments re the purposes in 2 CSS codes and 1 javascript code

--- FormWithValidationJava.js
+++ FormWithValidationJava.js
@@ -1,20 +1,22 @@
 function validate(){
     let errors = " ";
 
-//Get the value of the user input and copy it into variable
+//get the value of the user input and copy it into variable
     let firstName = document.getElementById("firstName").value;
-//Condition
+//condition
     if(firstName == ""){
 errors = errors+"<li>Please enter your First Name</li>"
 document.getElementById("firstName").classList.add("inputError");
     }
 
+    // get last name and check if it's empty
 let lastName = document.getElementById("lastName").value;
     if (lastName == ""){
         errors = errors + "<li>Please enter your Last Name</li>"
         document.getElementById("lastName").classList.add("inputError");
     }
 
+      // check if one radio button is selected
  let age = document.getElementsByName('age');
  let ageSelected = false;
  for(let i=0; i < age.length; i++){
@@ -27,44 +29,48 @@ let lastName = document.getElementById("lastName").value;
     document.getElementById("age").classList.add("inputError");
  }
 
-
+ // record email or check if it's empty
 let email = document.getElementById("email").value;
     if (email == ""){
         errors = errors + "<li>Please enter your Email</li>";
         document.getElementById("email").classList.add("inputError");
     }
 
+ // record phone nr or check if it's emptyy
 let phone = document.getElementById("phone").value;
     if (phone == ""){
         errors = errors + "<li>Please enter your Phone Number</li>";
         document.getElementById("phone").classList.add("inputError");
     }
 
+// check if a dept is chosen from dropdown options
 let department = document.getElementById("department").value;
     if (department == ""){
         errors = errors + "<li>Please specify the Department</li>";
         document.getElementById("department").classList.add("inputError");
     }
 
+// record subject or check if no characters were input
 let subject = document.getElementById("subject").value;
     if (subject == ""){
         errors = errors + "<li>Please enter the Subject</li>";
         document.getElementById("subject").classList.add("inputError");
     }
 
+// see if a message has any input
 let message = document.getElementById("message").value;
     if (message == ""){
         errors = errors + "<li>Please enter your Message</li>";
         document.getElementById("message").classList.add("inputError");
     }
 
-//If errors are not empty, meaning if we have errors, add them to the HTML list and show it
+// check for input errors in the form. if yes, showe error message, if no, allow to submit form
     if(errors != ""){
         document.getElementById("errorList").innerHTML = errors;
         document.getElementById("errorList").classList.remove("hidden");
         return false;
     }
-//if user has no errors, hide the errorList again
+// if user has no errors, hide the errorList again
     else{
         document.getElementById("errorList").classList.add("hidden");
         return true;
